d60c254374eabb05647dd21e74c486720d1d4a86
Hryhoriev94 <y.hryhoriev.1994@gmail.com>
2022-12-04 20:28:51 +0100

    fix generate id, in AddUser component added Refs

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,35 +1,41 @@
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
-import React, {useState} from "react";
+import React, {useRef} from "react";
 import MD5 from "crypto-js/md5";
 
-const AddUser = (props) => {
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
 
-    const [enteredUsername, setEnteredUsername] = useState('')
-    const [enteredAge, setEnteredAge] = useState('')
+
+const AddUser = (props) => {
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
     const errorHandler = () => {
         const errorModal = {};
-        if(enteredUsername.trim().length === 0 && enteredAge.trim().length === 0) {
+        if(nameInputRef.current.value.trim().length === 0 && ageInputRef.current.value.trim().length === 0) {
             errorModal.title = 'The user does not have a name and age';
             errorModal.message = 'Please enter username and age';
             props.onErrorSubmit(errorModal);
             return;
         }
-        if(enteredUsername.trim().length === 0) {
+        if(nameInputRef.current.value.trim().length === 0) {
             errorModal.title = 'Name is not be empty';
             errorModal.message = 'Please, complete name of the user';
             props.onErrorSubmit(errorModal);
             return;
         }
-        if(enteredAge.trim().length === 0) {
+        if(ageInputRef.current.value.trim().length === 0) {
             errorModal.title = 'Age is not be empty';
             errorModal.message = 'Please, complete age of the user';
             props.onErrorSubmit(errorModal);
             return;
         }
-        if(enteredAge.trim().length === 0) {
+        if(ageInputRef.current.value.trim().length === 0) {
             errorModal.title = 'Age can not be smaller than 1';
             errorModal.message = 'Please enter a valid age';
             props.onErrorSubmit(errorModal);
@@ -40,36 +46,43 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if((enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) || +enteredAge < 1)
+        console.log(nameInputRef.current.value);
+        if((nameInputRef.current.value.trim().length === 0 ||
+            ageInputRef.current.value.trim().length === 0) ||
+            +ageInputRef.current.value < 1)
         {
             errorHandler();
             return;
         }
-        const id = MD5((Math.floor(Math.random() * 2**64) / Math.floor(Math.random() * 2**16)) * Math.floor(Math.random() * 2**8)).toString()
+        const id = MD5(nameInputRef.current.value +
+            ageInputRef.current.value + getRandomInt(2, 8) ** getRandomInt(2, 16) / getRandomInt(2, 65535) +
+            Date.now()).toString();
+
         props.onSaveUser({
-            name: enteredUsername,
-            age: enteredAge,
+            name: nameInputRef.current.value,
+            age: ageInputRef.current.value,
             id: id
         });
-        setEnteredUsername('');
-        setEnteredAge('');
-    }
-
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value)
-    }
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value)
+        nameInputRef.current.value = null;
+        ageInputRef.current.value = null;
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler}/>
+                <input
+                    type="text"
+                    id="username"
+                    ref={nameInputRef}
+                />
 
                 <label htmlFor="age">Age (Years)</label>
-                <input type="age" id="userAge" value={enteredAge} onChange={ageChangeHandler}/>
+                <input
+                    type="age"
+                    id="userAge"
+                    ref={ageInputRef}
+                />
 
                 <Button type={'submit'}>Add User</Button>
             </form>
