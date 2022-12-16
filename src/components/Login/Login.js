87d53e5a00f880e81b4cdb410e11356cd41a09d7
Hryhoriev94 <y.hryhoriev.1994@gmail.com>
2022-12-16 18:30:58 +0100

    Refactor authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT') {
@@ -31,7 +32,7 @@ const passwordReducer = (state, action) => {
   return {value: '', isValid: false}
 }
 
-const Login = (props) => {
+const Login = () => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -47,6 +48,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
+
+  const authContext = useContext(AuthContext);
 
 
   const {isValid: emailIsValid} = emailState;
@@ -97,7 +100,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authContext.onLogin(emailState.value, passwordState.value);
   };
 
   return (
